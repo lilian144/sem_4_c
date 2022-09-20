f072e706fa5692e7e62cddde907d8366d7caf82c
Alena <qlushenko36@gmail.com>
2022-09-20 23:19:20 +0300

    задача 25

--- задача_27/Program.cs
+++ задача_27/Program.cs
@@ -3,4 +3,23 @@
 // 82 -> 10
 // 9012 -> 12
 
+int SumNum(int number)
+{
+    int count = Convert.ToString(number).Length;
+    int res = 0;
+    int advance = 0;
+    for(int i = 0; i <= count; i++)
+    {
+        advance = number - number % 10;
+        res = res + (number - advance);
+        number = number / 10;
+    }
+    return res;
+}
 
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+int res = SumNum(number);
+Console.WriteLine(res);
