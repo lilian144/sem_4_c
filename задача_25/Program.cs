96c9490f8a38e718c9640952967409dc54e664ac
Alena <qlushenko36@gmail.com>
2022-09-20 21:44:15 +0300

    задача 25

--- задача_25/Program.cs
+++ задача_25/Program.cs
@@ -2,7 +2,23 @@
 // и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
-
 // Не использовать Math.Pow() и аналоги!
 
-Console.WriteLine("Hello, World!");
+int Result(int number, int degree)
+{
+    int res = 1;
+    for(int i = 1; i <= degree; i++)
+    {
+        res = res * number;
+    }
+    return res;
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите степень числа: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+int res = Result(number, degree);
+Console.WriteLine(res);
